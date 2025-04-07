761fb70873eb900630b7edc8082efae2224a0bde
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-04-07 10:52:06 +0530

    Added Empty recoData check

--- client/src/components/RecommendationCard.jsx
+++ client/src/components/RecommendationCard.jsx
@@ -2,11 +2,11 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { token } from "../constant/token"
 import { useNavigate } from "react-router-dom"
-export const CardContainer = ({ Poster="/default_tnail.png", Title, Type, Year }) => {
+export const CardContainer = ({ Poster = "/default_tnail.png", Title, Type, Year }) => {
     return (
         <div className="border p-3 h-full rounded-md">
             <div className="h-[80%] flex justify-center">
-                <img src={Poster === "N/A"?"/default_tnail.png":Poster} className="rounded-md w-full h-full" alt={Title} />
+                <img src={Poster === "N/A" ? "/default_tnail.png" : Poster} className="rounded-md w-full h-full" alt={Title} />
             </div>
             <h2 className="text-2xl text-lime-400">{Title}</h2>
             <div className="flex w-full items-center justify-between">
@@ -21,7 +21,7 @@ const RecommendationCard = () => {
 
     const navigate = useNavigate()
     const [recoData, setRecoData] = useState([])
-    const onClickCard = async(imdbId) => {
+    const onClickCard = async (imdbId) => {
         try {
             const response = await axios.get(`https://www.omdbapi.com/?i=${imdbId}&plot=full&apikey=${token}`)
             sessionStorage.setItem("clickedCardDetails", JSON.stringify(response.data))
@@ -31,28 +31,28 @@ const RecommendationCard = () => {
         }
     }
 
-   useEffect(() => {
-     const fetchRecommendations = async() => {
-        try {
-            const randomAssLetter = ["Dark", "Fast", "glass", "Write", "Someday", "There", "Where", "Somewhere", "From"]  //camel case cuz why not
-            const page = Math.floor(Math.random()*5)+1;
-            const randomLetter = randomAssLetter[Math.floor(Math.random()*randomAssLetter.length)]
-            const response = await axios.get(`https://www.omdbapi.com/?s=${randomLetter}&page=${page}&apikey=${token}`)
-            setRecoData(response.data.Search)
-            // console.log(response.data);
-        } catch (error) {
-            console.error("Encountered an error", error);
+    useEffect(() => {
+        const fetchRecommendations = async () => {
+            try {
+                const randomAssLetter = ["Dark", "Fast", "glass", "Write", "Someday", "There", "Where", "Somewhere", "From"]  //camel case cuz why not
+                const page = Math.floor(Math.random() * 5) + 1;
+                const randomLetter = randomAssLetter[Math.floor(Math.random() * randomAssLetter.length)]
+                const response = await axios.get(`https://www.omdbapi.com/?s=${randomLetter}&page=${page}&apikey=${token}`)
+                setRecoData(response.data.Search)
+                // console.log(response.data);
+            } catch (error) {
+                console.error("Encountered an error", error);
+            }
         }
-     }
-     fetchRecommendations()
-   }, [])
-   
+        fetchRecommendations()
+    }, [])
+
 
     return (
         <div>
             <div className="px-3 py-1 grid md:grid-cols-3 grid-cols-1 gap-x-3 gap-y-6">
-                {recoData.map((value,index)=>(
-                    <div key={index} onClick={()=>{onClickCard(value.imdbID)}} className=" hover:scale-105 transition-all duration-200 backdrop-blur-2xl">
+                {recoData ? recoData.map((value, index) => (
+                    <div key={index} onClick={() => { onClickCard(value.imdbID) }} className=" hover:scale-105 transition-all duration-200 backdrop-blur-2xl">
                         <CardContainer
                             Poster={value.Poster}
                             Title={value.Title}
@@ -60,7 +60,9 @@ const RecommendationCard = () => {
                             Type={value.Type}
                         />
                     </div>
-                ))}
+                )) :
+                    <div>Recommendation Not Available</div>
+                }
             </div>
         </div>
     )
