a48ee53d0cc7fb0360d5c5814772326907942aeb
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-04-07 09:48:24 +0530

    Improved Readability using Arrays And objects for rendering details

--- client/src/components/SearchCards.jsx
+++ client/src/components/SearchCards.jsx
@@ -5,76 +5,63 @@ const SearchCards = () => {
     const navigate = useNavigate()
     const [seshData, setSeshData] = useState({})
     useEffect(() => {
-      const data = sessionStorage.getItem("clickedCardDetails")
-      setSeshData(JSON.parse(data))
+        const data = sessionStorage.getItem("clickedCardDetails")
+        setSeshData(JSON.parse(data))
     }, [])
-    
+
+    const details = [
+        { label: "Type", key: "Type" },
+        { label: "Language", key: "Language" },
+        { label: "Seasons", key: "totalSeasons" },
+        { label: "Released", key: "Released" },
+        { label: "Runtime", key: "Runtime" },
+        { label: "Genre", key: "Genre" },
+        { label: "Rated", key: "Rated" },
+        { label: "Writer", key: "Writer" },
+        { label: "Actors", key: "Actors" },
+        { label: "Box Office", key: "BoxOffice" },
+        { label: "IMDB Rating", key: "imdbRating" },
+        { label: "IMDB Votes", key: "imdbVotes" },
+    ]
+
+
     return (
         <div className="w-[90%] mx-auto">
-            <div className="flex my-2 justify-between items-center">
+            {seshData && <div className="flex my-2 justify-between items-center">
                 <h2 className="text-3xl font-semibold text-green-400">{seshData.Title}</h2>
                 <h2 className="text-2xl font-semibold">Country: {seshData.Country}</h2>
             </div>
-            <div className="xl:flex gap-5">
-                <img className="rounded-sm mx-auto xl:mx-0 border-amber-900 border aspect-auto object-cover h-[60vh] object-center" src={seshData.Poster} alt="poster" />
-                <div>
-                    <p className="text-xl text-blue-400"><span className="font-semibold text-2xl text-cyan-600">Summary: </span>{seshData.Plot}</p>
-                    <div className="grid grid-cols-2 my-3 gap-4">
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Type: </span>
-                            {seshData.Type}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Language: </span>
-                            {seshData.Language}
-                        </div>
-                        {seshData.totalSeasons &&
-                            <div className="text-xl text-purple-400">
-                                <span className="font-semibold text-2xl text-cyan-600">Seasons: </span>
-                                {seshData.totalSeasons}
-                            </div>
-                        }
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Released: </span>
-                            {seshData.Released}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Runtime: </span>
-                            {seshData.Runtime}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Genre: </span>
-                            {seshData.Genre}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Rated: </span>
-                            {seshData.Rated}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Writer: </span>
-                            {seshData.Writer}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Actors: </span>
-                            {seshData.Actors}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">Box Office: </span>
-                            {seshData.BoxOffice}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">IMDB Rating: </span>
-                            {seshData.imdbRating}
-                        </div>
-                        <div className="text-xl text-purple-400">
-                            <span className="font-semibold text-2xl text-cyan-600">IMDB votes: </span>
-                            {seshData.imdbVotes}
-                        </div>
+            }
+            {seshData &&
+                <div className="xl:flex gap-5">
+                    <img className="rounded-sm mx-auto xl:mx-0 border-amber-900 border aspect-auto object-cover h-[60vh] object-center" src={seshData.Poster} alt="poster" />
+                    <div>
+                        <p className="text-xl text-blue-400">
+                            <span className="font-semibold text-2xl text-cyan-600">Summary: </span>
+                            {seshData?.Plot || "Summary Not Available"}
 
+                        </p>
+
+                        <div className="grid grid-cols-2 my-3 gap-4">
+                            {details.map(({ label, key }) => {
+                                return seshData?.[key] && (
+                                    <div key={key} className="text-xl text-purple-400">
+                                        <span className="font-semibold text-2xl text-cyan-600">{label}: </span>
+                                        {seshData[key]}
+                                    </div>
+                                )
+                            })}
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            <button onClick={()=>{navigate("/")}} className="px-4 py-2 border m-3 rounded-lg">Home</button>
+            }
+            {!seshData &&
+                <div>
+                    No Data Available, Click <a href="/" onClick={() => { sessionStorage.removeItem("clickedCardDetails") }}>Here</a> To Go Back
+                </div>
+            }
+            <button onClick={() => { navigate("/") }} className="px-4 py-2 border m-3 rounded-lg">Home</button>
         </div>
     )
 }
